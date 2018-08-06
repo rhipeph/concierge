29ef37c2de98b5e34461d7e31fa1b20adab6bb1d
rhipeph <matt@mattgaralde.com>
2018-08-07 04:54:15 +0800

    new

--- script.js
+++ script.js
@@ -1,26 +1,9 @@
-/*var ip = 'check';
-var access_key = 'f5ba7b457ae7b68d069be7ac12b492ab';
-
-$.ajax({
-    url: 'http://api.ipstack.com/' + ip + '?access_key=' + access_key,   
-    dataType: 'jsonp',
-    success: function(json) {
-        var myPublicIP = json.ip;
-        if(myPublicIP == "223.130.19.2") {
-            window.location.href = "portal.html";
-        } else {
-            window.location.href = "error.html";
-        }                   
-    }
-});*/
-
-
 function getIP(json) {
-    var myPublicIP = json.query;
+   var myPublicIP = json.query;
         console.log(myPublicIP);
         if(myPublicIP == "223.130.19.2") {
-            window.location.href = "portal.html";
+           $("#errorbox").hide( "fast" );
         } else {
-            window.location.href = "error.html";
+            $("#powerapps").hide( "fast" );
         }   
    }
