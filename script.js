9d622240f95ec13f01cebf1908065128e4bd6444
rhipeph <42110041+rhipeph@users.noreply.github.com>
2018-08-07 18:38:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function getIP(json) {
    var myPublicIP = json.query;
-        console.log(myPublicIP);
         if(myPublicIP == "223.130.19.2") {
            $("#errorbox").hide( "fast" );
         } else {
